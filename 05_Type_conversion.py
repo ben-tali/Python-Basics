def4b3821952de7ef631a760c82e0eeb9594ef5b
ben-tali <85305670+ben-tali@users.noreply.github.com>
2021-06-10 02:48:35 +0300

    assighnment

--- 05_Type_conversion.py
+++ 05_Type_conversion.py
@@ -1,8 +1,34 @@
 # write a program to get the year you were born in
+#yob = input("Tell me your year of birth")
 
 
 # calculate the age
+#age = 2021 - yob
+#print(f"You are {age} old")
+#print(type(yob))
+#age = 2021 - int(input("Tell me your year of birth"))
 
 
 # write a program to ask  a user to enter their weight in pounds and convert it into a kg
 # 1lb = 0.45kg
+change_to_kilograms = True
+change_to_pounds = True
+
+pounds = float(input('Enter weight in Pounds(Lbs) to Convert into Kilograms:'))
+kilo_grams = pounds * 0.453592
+
+
+kilograms = float(input("Enter weight in kilograms(Kgs) to convert into Pounds:"))
+pounds = kilograms / 0.453592
+
+if change_to_kilograms and change_to_pounds:
+    print(pounds," you (Lbs) are", kilo_grams, "Kilograms (Kgs)")
+else: 
+     print(kilograms,"you (Kgs) are", pounds, "Pounds (Lbs)") 
+
+
+
+
+
+
+
--- 07_loops.py
+++ 07_loops.py
@@ -1,10 +1,22 @@
 
 # while loop
+#a = 0
+#while a < 10:
+ #   print(a)
+  #  a+=1
 
 
 # for loops
 
 # loop over an array [list]
+#languages = ["PHP", "PYTHON", "JAVA", "DART"]
+#for lang in languages:
+ #   print("lang")
+Marks = (60, 80, 85, 78, 60, 92)
+t = sum(Marks)
+average = t / 6
+print(t, (average))
 
 
-# find the total price
+
+# find the total pric
